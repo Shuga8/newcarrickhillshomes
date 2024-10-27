77082e6e597bc786e032a1416e503066f1792a35
Shuga8 <lotocharles8@gmail.com>
2024-10-27 17:58:17 +0100

    ready for design, ui

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,11 @@
         <x-seo :title="$title" />
         <title>{{ config('app.name') }} | {{ $title }}</title>
 
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+            integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+
         @vite('resources/js/app.js')
     </head>
 
