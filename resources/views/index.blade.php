6cf89270cb7f9b1ca1a8e99266dc35329b31c0a7
Shuga8 <lotocharles8@gmail.com>
2024-10-30 18:02:31 +0100

    subscribe form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -437,21 +437,21 @@
         </div>
     </section>
 
-    <section class="subcribe-form w-full h-[30dvh] px-3 flex justify-center items-center">
+    <section class="subcribe-form w-full h-[40dvh] px-8 flex flex-col gap-y-10 justify-center items-center">
 
-        <form action="{{ route('subscribe') }}" method="POST">
+        <h4 class="old-standard text-[1.5rem] md:text-[60px] text-slate-800 dark:text-white text-center font-[450]">
+            Subscribe to Newsletter
+        </h4>
+
+        <form action="{{ route('subscribe') }}" method="POST" class="w-full flex justify-center">
             @csrf
 
-            <h4 class="text-yellow-600 dark:text-yellow-400 text-[13px] font-[450] mb-3">Subscribe to our newsletter
-            </h4>
-
-            <div class="input w-[280px] md:w-[360px] relative">
+            <div class="input w-[100%] md:w-[70%] lg:w-[60%] relative">
                 <input type="text" name="email" id="email"
-                    class="py-[10px] px-[6px] border-blue-600 border text-black dark:text-white dark:border-white focus:ring-0 bg-transparent w-full text-[13px] rounded-sm"
+                    class="py-[24px] px-[24px] w-full rounded-sm border-transparent bg-slate-200 focus:border-none focus:outline-none text-[#121212] text-[11px] md:text-[14px]"
                     placeholder="email address...">
                 <button type="submit"
-                    class="absolute top-[50%] right-[2px] translate-y-[-50%] text-white bg-blue-700 dark:text-white dark:bg-blue-500 pt-1.5 pb-1 px-2.5 rounded-sm"><i
-                        class="fi fi-sr-paper-plane-top"></i></button>
+                    class="absolute top-[50%] right-[10px] translate-y-[-50%] text-white bg-blue-700 dark:text-white dark:bg-blue-800 py-[14px] px-5 md:px-10 rounded-sm hover:bg-stone-800 dark:hover:bg-slate-700 hover:transition-all ease-out duration-300">Subscribe</button>
             </div>
         </form>
     </section>
