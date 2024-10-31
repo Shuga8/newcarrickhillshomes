e329da8d21999a59f34e473202ff4e43c04e2819
Shuga8 <lotocharles8@gmail.com>
2024-10-31 18:59:55 +0100

    apartments page

--- resources/views/apartments.blade.php
+++ resources/views/apartments.blade.php
@@ -17,7 +17,7 @@
     </section>
 
     <section
-        class="apartments py-8 md:py-32 px-3 flex flex-wrap justify-center gap-x-12 gap-y-10 bg-gray-100 dark:bg-[#1212]">
+        class="apartments py-8 md:py-32 px-3 flex flex-wrap justify-center gap-x-14 gap-y-14 bg-gray-100 dark:bg-[#1212]">
 
         <div
             class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
@@ -49,11 +49,310 @@
                 </div>
 
                 <button
-                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-5 uppercase">Book
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
                     Now</button>
             </div>
         </div>
 
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3657.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3667.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3675.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3620.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3679.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3623.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3639.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+        <div
+            class="apartment relative bg-white basis-[360px] h-[489.2px] rounded-[2px] overflow-hidden grow shrink md:shrink-0 md:grow-0">
+
+            <div class="img w-full h-[240px] overflow-hidden">
+                <img src="{{ asset('assets/IMG_3669.jpeg') }}" class="w-full h-full" alt="">
+            </div>
+
+            <div
+                class="apartment-facilities bg-white  w-4/5 h-[52.4px] absolute z-[2] top-[49%] left-1/2 translate-x-[-50%] translate-y-[-49%] flex flex-row gap-x-3 justify-center items-center text-[14px] text-gray-700">
+                <i class="fi fi-rr-bed"></i>
+                <i class="fi fi-ss-wifi"></i>
+                <i class="fi fi-ts-car-alt"></i>
+                <i class="fi fi-rr-mug-hot-alt"></i>
+                <i class="fi fi-rr-room-service"></i>
+                <i class="fi fi-ts-gamepad"></i>
+                <i class="fi fi-rr-swimmer"></i>
+            </div>
+
+            <div class="content w-full h-[249.2px] bg-white flex flex-col gap-y-6 py-10 px-3">
+                <a href=""
+                    class="w-fit mx-auto old-standard text-[1.3rem] text-gray-950 dark:text-stone-700 font-[450] dark:font-semibold">Minimal
+                    Duplex
+                    Apartment</a>
+
+                <div class="w-full px-2  text-center text-[0.75rem] text-slate-600 leading-[1.4]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, repudiandae. Lorem ipsum dolor sit
+                    amet.
+                </div>
+
+                <button
+                    class="px-8 py-3.5 border-[2px] font-medium transition-colors ease-in-out duration-300 border-cyan-600 text-cyan-600 hover:bg-cyan-600 hover:text-white w-fit mx-auto mt-3.5 uppercase">Book
+                    Now</button>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="subcribe-form w-full h-[40dvh] px-8 flex flex-col gap-y-10 justify-center items-center">
+
+        <h4 class="old-standard text-[1.5rem] md:text-[60px] text-slate-800 dark:text-white text-center font-[450]">
+            Subscribe to Newsletter
+        </h4>
+
+        <form action="{{ route('subscribe') }}" method="POST" class="w-full flex justify-center">
+            @csrf
+
+            <div class="input w-[100%] md:w-[70%] lg:w-[60%] relative">
+                <input type="text" name="email" id="email"
+                    class="py-[24px] px-[24px] w-full rounded-sm border-transparent bg-slate-200 focus:border-none focus:outline-none text-[#121212] text-[11px] md:text-[14px]"
+                    placeholder="email address...">
+                <button type="submit"
+                    class="absolute top-[50%] right-[10px] translate-y-[-50%] text-white bg-blue-700 dark:text-white dark:bg-blue-800 py-[14px] px-5 md:px-10 rounded-sm hover:bg-stone-800 dark:hover:bg-slate-700 hover:transition-all ease-out duration-300">Subscribe</button>
+            </div>
+        </form>
     </section>
 
 </x-layout>
