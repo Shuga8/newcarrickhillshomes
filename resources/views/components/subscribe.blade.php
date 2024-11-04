064c2f71afa3349600b28c8226dd8a1bd00a0f7a
Shuga8 <lotocharles8@gmail.com>
2024-11-04 12:29:42 +0100

    Subscription componenet

---
+++ resources/views/components/subscribe.blade.php
@@ -0,0 +1,18 @@
+    <section class="subcribe-form w-full h-[40dvh] px-8 flex flex-col gap-y-10 justify-center items-center">
+
+        <h4 class="old-standard text-[1.5rem] md:text-[60px] text-slate-800 dark:text-white text-center font-[450]">
+            Subscribe to Newsletter
+        </h4>
+
+        <form action="{{ route('subscribe') }}" method="POST" class="w-full flex justify-center">
+            @csrf
+
+            <div class="input w-[100%] md:w-[70%] lg:w-[60%] relative">
+                <input type="text" name="email" id="email"
+                    class="py-[24px] px-[24px] w-full rounded-sm border-transparent bg-slate-200 focus:border-none focus:outline-none text-[#121212] text-[11px] md:text-[14px]"
+                    placeholder="email address..." required>
+                <button type="submit"
+                    class="absolute top-[50%] right-[10px] translate-y-[-50%] text-white bg-blue-700 dark:text-white dark:bg-blue-800 py-[14px] px-5 md:px-10 rounded-sm hover:bg-stone-800 dark:hover:bg-slate-700 hover:transition-all ease-out duration-300">Subscribe</button>
+            </div>
+        </form>
+    </section>
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer w-full">
 
-    <div class="py-10 md:py-16 px-4 md:px-10"></div>
+    <div class="py-10 md:py-16 px-4 md:px-10 bg-gray-200 dark:bg-slate-800"></div>
 
-    <div class="w-full text-center old-standard bg-[#1a132b] text-[13px] py-3 ">
-        New Carrick Hills Homes&trade; &copy; <span>
+    <div class="w-full text-center bg-[#1a132b] text-[11px] py-3 ">
+        New Carrick Hills Homes&trade; &copy; <span class="italic"> OneThought Technologies
             <script>
                 document.write(new Date().getFullYear())
             </script>
